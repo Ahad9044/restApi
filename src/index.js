9128ac333e50b0e920afae90ba744e18557c74b5
Your Name <syd.ahad@gmail.com>
2025-11-05 17:47:57 +0530

    next handler

--- src/index.js
+++ src/index.js
@@ -5,27 +5,16 @@ const PORT = 1111
 const app = express()
 
 // params 
-app.get("/abc/:userId/:password",(req , res)=>{
-   console.log(req.params);
-   
-    res.send("hello")
-})
-
-//query 
-// In query , we get parameters through req body
-app.get("/abc",(req , res)=>{
-   console.log(req.query);
-    res.send("hello")
-})
-
-
-// app.use("/test2",(req , res)=>{
-//     res.send("hello2")
-// })
-// app.use("/test3",(req , res)=>{
-//     res.send("hello3")
-// })
-
+app.get("/abc", 
+    (req, res, next) => {
+        console.log(req.params); // {}
+        next(); // passes control to the next handler
+    }, 
+    (req, res) => {
+        console.log(req.query); 
+        res.send("hello2");
+    }
+);
 
 app.listen(PORT, () => {
     console.log(`App is listening on port ${PORT}`);
